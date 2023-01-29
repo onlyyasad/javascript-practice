04b21baf7518d07747e8066c3236bb6c1a853be2
onlyyasad <onlyyasad@gmail.com>
2023-01-29 17:40:40 +0600

    loop added

---
+++ loop.js
@@ -0,0 +1,28 @@
+var number = 0;
+while (number <39) {
+    // console.log('Ajke amar mon valo nei')
+    number++;
+}
+
+var num1 = 58;
+while (num1 <= 98) {
+    // console.log(num1)
+    num1++;
+}
+
+var num2 = 412;
+while (num2 <= 456) {
+    // console.log(num2)
+    num2 += 2;
+}
+
+for (i = 0; i <= 10; i +=2) {
+    // console.log(i)
+}
+
+for (i = 30; i <= 80; i++) {
+    if (i > 44) {
+        continue;
+    }
+    console.log(i)
+}
